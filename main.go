82c8a4b9c6c4c846979a5bf7a5aed83b1162bf4d
Fauzan Amir Al Ghiffary <fauzanamiralghiffary@MacBook-Pro-Fauzan.local>
2021-09-08 22:39:31 +0700

    tugas operator

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rupiahToDollar(money float64) string {
+	konversi := (money/14251.55)
+	return fmt.Sprintf("USD %f",konversi)
+}
+
+func rupiahToEuro(money float64) string {
+	konversi := (money/16846.72)
+	return fmt.Sprintf("%f EURO",konversi)
+}
+
+func bonus(gbp int) string {
+	var knut int
+	var sickle int
+	var si int
+	var galleon int
+	var galleons int
+	knut = gbp*100
+	sickle = (knut%29)
+	si = knut/29
+	galleon = (si/17)
+	galleons = (si%17)
+	
+	return fmt.Sprintf(`Jumlah Knut yang kamu dapat : %d
+	Hasil penukaran yang didapat : %d galleon(s)
+	Sisa ditukar menjadi : %d sickle(s)
+	Keping knut yang tersisa %d`, knut,galleon,galleons,sickle)
+	//return fmt.Sprintf("%d",galleon)
+}
+
+func main() {
+	var i int
+	var nominal float64
+
+	var gbp int
+
+	fmt.Printf("Pilih:\n")
+	fmt.Printf("1. Rupiah to Dollar:\n")
+	fmt.Printf("2. Rupiah to Euro :\n")
+	fmt.Printf("3. Bonus :\n")
+	fmt.Scanf("%d",&i)
+
+	switch i{
+		case 1:
+			fmt.Print("Tulis nominal : ")
+			fmt.Scan(&nominal)		
+			fmt.Println(rupiahToDollar(nominal))
+		case 2:
+			fmt.Print("Tulis nominal : ")
+			fmt.Scan(&nominal)
+			fmt.Println(rupiahToEuro(nominal))
+		case 3:
+			fmt.Print("GBP : ")
+			fmt.Scan(&gbp)
+			fmt.Println(bonus(gbp))
+		default:fmt.Println("Tidak ada")
+	}
+}
+
